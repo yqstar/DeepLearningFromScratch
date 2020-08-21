4587d7d3eda7d3c68da68422250f7f205696c922
AndrewYq <1656302851@qq.com>
2020-08-21 19:51:22 +0800

    Update DeepNerualNetwork.py

--- DeepNerualNetwork.py
+++ DeepNerualNetwork.py
@@ -20,15 +20,24 @@
 import numpy as np
 import time
 from DataProcess import DataProcess
-from Utils import Utils
 
 np.random.seed(0)
 
 
-def softmax(array):
-    """Compute the softmax of vector array."""
-    exps = np.exp(array)
-    return exps / np.sum(exps)
+# def softmax(array):
+#     """Compute the softmax of vector array."""
+#     exps = np.exp(array)
+#     return exps / np.sum(exps)
+
+def softmax(X):
+    X_exp = np.exp(X)
+    partition = X_exp.sum(axis=1, keepdims=True)
+    return X_exp / partition  # 这里应用了广播机制。
+
+
+# def softmax(x):
+#     max = np.max(x)
+#     return np.exp(x - max) / sum(np.exp(x - max))
 
 
 def der_softmax_cross_entropy(act_array, pre_array):
@@ -77,13 +86,6 @@ def der_relu(x):
 
 def onehot(num_class, label_array):
     return np.eye(num_class)[label_array]
-    # max_label = np.max(label_array)
-    # if max_label < num_class:
-    #     tmp = np.eye(num_class)[label_array]
-    # else:
-    #     print("标签数据有误")
-    #     pass
-    # return tmp
 
 
 class DeepNeuralNetwork(object):
@@ -96,47 +98,64 @@ class DeepNeuralNetwork(object):
         self.b1 = np.random.rand(self.num_sample, 1)
         self.w2 = np.random.rand(self.num_hidden, self.num_out)
         self.b2 = np.random.rand(self.num_sample, 1)
-
-    # def __str__(self):
-    #     pass
+        print("test")
 
     def forward(self, feature_array):
         A1 = np.dot(feature_array, self.w1) + self.b1
         Z1 = sigmoid(A1)
         A2 = np.dot(Z1, self.w2) + self.b2
         Z2 = softmax(A2)
-        # hidden_layer = Utils.sigmoid(np.dot(feature_array, self.w1) + self.b1)
-        # out_layer = Utils.softmax(np.dot(hidden_layer, self.w2) + self.b2)
+
         cache = {"Z1": Z1,
                  "A1": A1,
                  "Z2": Z2,
                  "A2": A2}
         return Z2, cache
 
-    def backward(self, feature_array, label_array, lr=0.001, iteration=50):
+    def backward(self, feature_array, label_array, learning_rate=0.1):
+        # m = feature_array.shape[1]
+        # print(m)
         Z2, cache = self.forward(feature_array)
         # 获取Z1和Z2
         Z1 = cache['Z1']
         Z2 = cache['Z2']
+        A1 = cache['A1']
+        A2 = cache['A2']
         print("Z1 shape", Z1.shape)
         print("Z2 shape", Z2.shape)
+        print("A1 shape", A1.shape)
+        print("A2 shape", A2.shape)
 
         label_onehot = onehot(self.num_out, label_array)
-        dZ2_A2 = der_softmax_cross_entropy(label_onehot, Z2)
-        print("dZ2_A2 shape", dZ2_A2.shape)
-        dW2 = np.dot(Z1.T, dZ2_A2)
+        dL_A2 = der_softmax_cross_entropy(label_onehot, Z2)
+        print("dL_A2 shape", dL_A2.shape)
+        # dW2 = 1/m * np.dot(Z1.T, dL_A2)
+        m = dL_A2.shape[1]
+        dW2 = np.dot(Z1.T, dL_A2)
+        # db2 = 1 / m * np.sum(dL_A2, axis=1, keepdims=True)
+        db2 = np.sum(dL_A2, axis=1, keepdims=True)
 
-        db2 = np.sum(dZ2_A2, axis=1, keepdims=True)
+        # print("w2 shape", self.w2.shape)
+        dL_A1 = np.dot(dL_A2, self.w2.T) * der_sigmoid(A1)
+        print("dL_A1 shape", dL_A1.shape)
+        n = dL_A1.shape[1]
+        # dW1 = 1/m * np.dot(feature_array.T, dL_A1)
+        dW1 = np.dot(feature_array.T, dL_A1)
+        # db1 = 1 / n * np.sum(dL_A1, axis=1, keepdims=True)
+        db1 = np.sum(dL_A1, axis=1, keepdims=True)
 
-        print("w2 shape", self.w2.shape)
-        dZ2_A1 = np.dot(dZ2_A2, self.w2.T) * (1 - np.dot(dZ2_A2, self.w2.T))
-        dW1 = np.dot(feature_array.T, dZ2_A1)
-        db1 = np.sum(dZ2_A1, axis=1, keepdims=True)
+        # 参数更新
+        self.w1 -= dW1 * learning_rate
+        self.b1 -= db1 * learning_rate
+        self.w2 -= dW2 * learning_rate
+        self.b2 -= db2 * learning_rate
 
         grads = {"dW1": dW1,
                  "db1": db1,
                  "dW2": dW2,
-                 "db2": db2}
+                 "db2": db2,
+                 "dL_A2": dL_A2,
+                 "dL_A1": dL_A1}
         return grads
 
     def update_parameters(self, grads, learning_rate=1.2):
@@ -145,200 +164,33 @@ class DeepNeuralNetwork(object):
         db1 = grads['db1']
         dW2 = grads['dW2']
         db2 = grads['db2']
+
         # 参数更新
         self.w1 -= dW1 * learning_rate
         self.b1 -= db1 * learning_rate
         self.w2 -= dW2 * learning_rate
         self.b2 -= db2 * learning_rate
 
-        # label_onehot = onehot(self.num_class, label_array)
-        # n_sample = len(feature_array)
-        # out_softmax = Utils.softmax(np.dot(Utils.sigmoid(np.dot(feature_array, self.w1) + self.b1), self.w2) + self.b2)
-        # loss_ce = np.dot(label_onehot, np.log(out_softmax))
-        #
-        # out_hidden = Utils.sigmoid(np.dot(feature_array, self.w1) + self.b1)
-        #
-        # dL_ce_softmax = der_softmax_cross_entropy(10, label_onehot, out_softmax)
-        #
-        # dw2 = np.dot(Utils.sigmoid(np.dot(feature_array, self.w1) + self.b1).T, dL_ce_softmax) // 4 * 2
-        # db2 = dL_ce_softmax
-        # # dw1 = dw2 *
-        # dz1 = der_sigmoid(np.dot(feature_array, self.w1) + self.b1)
-        # # dw1 = np.dot(dw2 *, )
-        # # db1 =
-        #
-        # return True
-
 
 if __name__ == '__main__':
     start = time.time()
     # 加载数据
     # 获取训练集及标签
-    trainData, trainLabel = DataProcess.load_data('./data/Mnist/mnist_train.csv')
+    trainData, trainLabel = DataProcess.load_data('./data/Mnist/mnist_train.csv', binary_classification=False)
     # 获取测试集及标签
-    testData, testLabel = DataProcess.load_data('./data/Mnist/mnist_test.csv')
+    # testData, testLabel = DataProcess.load_data('./data/Mnist/mnist_test.csv',binary_classification=False)
     # 实例化感知机，输入特征为4
     dnn = DeepNeuralNetwork(trainData, trainLabel, 64, 10)
-    for i in range(50):
+    for i in range(1):
         grads = dnn.backward(trainData, trainLabel)
-        dnn.update_parameters(grads, learning_rate=0.01)
+        # dnn.update_parameters(grads, learning_rate=0.01)
+        print(dnn.w1.max())
         label_pre, _ = dnn.forward(trainData)
-        loss_ce = np.dot(onehot(10,trainLabel), np.log(label_pre))
+        loss_ce = -np.mean(np.sum(np.multiply(onehot(10, trainLabel), np.log(label_pre)), axis=1))
         acc = np.sum(trainLabel == np.argmax(label_pre, axis=1)) / trainLabel.shape[0]
+        # if not i % 50:
         print("Iter", i, "Loss", loss_ce, "Acc", acc)
 
-    # for i in range(100):
-    #     dnn.backward()
-    #     acc = np.sum(dnn.y == dnn.output) / dnn.y.shape[0]
-    #     print(acc)
-    # # 训练模型，获得计算参数
-    # weight, bias = dnn.forward(trainData, trainLabel, iteration=1000)
-    # # 预测结果
-    # y_pre = dnn.backward(testData)
     end = time.time()
     # 显示用时时长
     print('time span:', end - start)
-
-#
-#
-# class NeuralNetwork:
-#     def __init__(self, x, y):
-#         self.input = x
-#         self.weights1 = np.random.rand(self.input.shape[1], 4)
-#         self.layer1 = Utils.sigmoid(np.dot(self.input, self.weights1))
-#         self.weights2 = np.random.rand(4, 1)
-#         self.y = y
-#         self.output = np.zeros(self.y.shape)
-#
-#     def forward(self):
-#         self.layer1 = Utils.sigmoid(np.dot(self.input, self.weights1))
-#         self.output = Utils.sigmoid(np.dot(self.layer1, self.weights2))
-#
-#     def backward(self):
-#         # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
-#         d_weights2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * Utils.sigmoid_derivative(self.output)))
-#         d_weights1 = np.dot(self.input.T, (
-#                 np.dot(2 * (self.y - self.output) * Utils.sigmoid_derivative(self.output),
-#                        self.weights2.T) * Utils.sigmoid_derivative(self.layer1)))
-#         # update the weights with the derivative (slope) of the loss function
-#         self.weights1 += d_weights1
-#         self.weights2 += d_weights2
-#
-# from math import exp
-# from random import seed
-# from random import random
-#
-#
-# # Initialize a network
-# def initialize_network(n_inputs, n_hidden, n_outputs):
-#     network = list()
-#     hidden_layer = [{'weights': [random() for i in range(n_inputs + 1)]} for i in range(n_hidden)]
-#     network.append(hidden_layer)
-#     output_layer = [{'weights': [random() for i in range(n_hidden + 1)]} for i in range(n_outputs)]
-#     network.append(output_layer)
-#     return network
-#
-#
-# # Calculate neuron activation for an input
-# def activate(weights, inputs):
-#     activation = weights[-1]
-#     for i in range(len(weights) - 1):
-#         activation += weights[i] * inputs[i]
-#     return activation
-#
-#
-# # Transfer neuron activation
-# def transfer(activation):
-#     return 1.0 / (1.0 + exp(-activation))
-#
-#
-# # Forward propagate input to a network output
-# def forward_propagate(network, row):
-#     inputs = row
-#     for layer in network:
-#         new_inputs = []
-#         for neuron in layer:
-#             activation = activate(neuron['weights'], inputs)
-#             neuron['output'] = transfer(activation)
-#             new_inputs.append(neuron['output'])
-#         inputs = new_inputs
-#     return inputs
-#
-#
-# # Calculate the derivative of an neuron output
-# def transfer_derivative(output):
-#     return output * (1.0 - output)
-#
-#
-# # Backpropagate error and store in neurons
-# def backward_propagate_error(network, expected):
-#     for i in reversed(range(len(network))):
-#         layer = network[i]
-#         errors = list()
-#         if i != len(network) - 1:
-#             for j in range(len(layer)):
-#                 error = 0.0
-#                 for neuron in network[i + 1]:
-#                     error += (neuron['weights'][j] * neuron['delta'])
-#                 errors.append(error)
-#         else:
-#             for j in range(len(layer)):
-#                 neuron = layer[j]
-#                 errors.append(expected[j] - neuron['output'])
-#         for j in range(len(layer)):
-#             neuron = layer[j]
-#             neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
-#
-#
-# # Update network weights with error
-# def update_weights(network, row, l_rate):
-#     for i in range(len(network)):
-#         inputs = row[:-1]
-#         if i != 0:
-#             inputs = [neuron['output'] for neuron in network[i - 1]]
-#         for neuron in network[i]:
-#             for j in range(len(inputs)):
-#                 neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]
-#             neuron['weights'][-1] += l_rate * neuron['delta']
-#
-#
-# # Train a network for a fixed number of epochs
-# def train_network(network, train, l_rate, n_epoch, n_outputs):
-#     for epoch in range(n_epoch):
-#         sum_error = 0
-#         for row in train:
-#             outputs = forward_propagate(network, row)
-#             expected = [0 for i in range(n_outputs)]
-#             expected[row[-1]] = 1
-#             sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
-#             backward_propagate_error(network, expected)
-#             update_weights(network, row, l_rate)
-#         print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch, l_rate, sum_error))
-
-
-# Test training backprop algorithm
-# seed(1)
-# dataset = [[2.7810836, 2.550537003, 0],
-#            [1.465489372, 2.362125076, 0],
-#            [3.396561688, 4.400293529, 0],
-#            [1.38807019, 1.850220317, 0],
-#            [3.06407232, 3.005305973, 0],
-#            [7.627531214, 2.759262235, 1],
-#            [5.332441248, 2.088626775, 1],
-#            [6.922596716, 1.77106367, 1],
-#            [8.675418651, -0.242068655, 1],
-#            [7.673756466, 3.508563011, 1]]
-# n_inputs = len(dataset[0]) - 1
-# n_outputs = len(set([row[-1] for row in dataset]))
-# network = initialize_network(n_inputs, 2, n_outputs)
-# train_network(network, dataset, 0.5, 20, n_outputs)
-# for layer in network:
-#     print(layer)
-
-# import torch
-# from torch import nn
-#
-#
-# class DNN(nn.Module):
-#     def __init__(self):
-#         super(DNN)
