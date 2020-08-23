f7927389c4dbbe6431fc7f6e20585256a79fe1ba
AndrewYq <1656302851@qq.com>
2020-08-23 21:47:32 +0800

    update rnn

---
+++ RecurrentNeuralNetworks.py
@@ -0,0 +1,3 @@
+# http://songhuiming.github.io/pages/2017/08/12/build-neural-network-from-scratch/
+# https://songhuiming.github.io/pages/2017/08/20/build-recurrent-neural-network-from-scratch/
+# https://songhuiming.github.io/category/python.html
